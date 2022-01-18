c307b7d997de4dce38045a551dd7b8809ff8ef63
Vedanta Somnathe <vedanta1412@gmail.com>
2022-01-19 00:24:24 +0530

    logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,11 +4,12 @@ import { FaShoppingBag } from "react-icons/fa";
 const Nav = ({ cart, toggleCartActive }) => {
     return (
         <nav className="nav w-screen bg-darkChocolate px-24 py-5 flex justify-between items-center shadow-xl">
-            <div className="logo w-1/3">Logo</div>
+            <div className="logo w-1/3">
+                <img src="/logo.svg" alt="h" className="w-20 h-10 rounded-xl" />
+            </div>
             <div className="links w-1/3 flex justify-between">
                 <Link to="/">Home</Link>
                 <Link to="/products">Products</Link>
-                <Link to="/create">Creation</Link>
             </div>
             <div className="cart w-1/3 flex justify-end">
                 <FaShoppingBag
